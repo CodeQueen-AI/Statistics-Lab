59d860bfbb0cba68196ccdd91ddad4ed42a43075
CodeQueen-AI <codeq209@gmail.com>
2026-01-27 00:57:01 +0500

    update

---
+++ 1-Descriptive Statistics/Mean.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+def my_sum(data):
+    result = 0
+    for value in data:
+        result += value
+    return result
+
+def mean(data):
+    return my_sum(data) / len(data)
+
+# Marks
+marks = [80, 50, 45, 91]
+print('Mean of Marks is : ' , mean(marks))
+
+# Using Numpy
+marks = [80, 50, 45, 91]
+print('Mean of Marks (Numpy) is : ' , np.mean(marks))
+
+# Using Pandas
+marks = [80, 50, 45, 91]
+marks_series = pd.Series(marks)
+print('Mean of Marks (Pandas) is : ' , marks_series.mean())
