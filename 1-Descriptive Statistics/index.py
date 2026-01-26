47921101611b8914b0cac0df5555a8f86c9fe5af
CodeQueen-AI <codeq209@gmail.com>
2026-01-27 00:45:15 +0500

    update

--- 1-Descriptive Statistics/index.py
+++ 1-Descriptive Statistics/index.py
@@ -14,10 +14,6 @@ def mean(data):
 marks = [80, 50, 45, 91]
 print('Mean of Marks is : ' , mean(marks))
 
-# Steps
-steps = [4000, 5000, 6000, 7000, 8000]
-print('Mean of Steps is : ', mean(steps))
-
 # Using Numpy
 marks = [80, 50, 45, 91]
 print('Mean of Marks (Numpy) is : ' , np.mean(marks))
