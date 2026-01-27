cd13e8b7a9e4baef11fd18623f72f0bc5fe55331
CodeQueen-AI <codeq209@gmail.com>
2026-01-27 18:17:26 +0500

    update

--- 2-Inferential Statistics/index.py
+++ 2-Inferential Statistics/index.py
@@ -16,3 +16,14 @@ print("Sample Space:", sample_space)
 # Event
 event = [2, 4, 6]
 print('Event :', event)
+
+def join_probability(event_A, event_B, total_outcomes):
+    join_event = list(set(event_A) & set(event_B))
+    return len(join_event) / total_outcomes
+
+event_A = [2, 4, 6]
+event_B = [4, 5, 6]
+total_outcomes = 6
+
+prob = join_probability(event_A, event_B, total_outcomes)
+print('Join Probability: ', prob)
