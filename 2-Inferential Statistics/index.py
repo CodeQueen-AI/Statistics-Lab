a6baddfd1cea4d54c7b7be7271c579ad5e3430ea
CodeQueen-AI <codeq209@gmail.com>
2026-01-27 18:20:57 +0500

    update

--- 2-Inferential Statistics/index.py
+++ 2-Inferential Statistics/index.py
@@ -29,3 +29,13 @@ total_outcomes = 6
 prob = join_probability(event_A, event_B, total_outcomes)
 print('Join Probability: ', prob)
 
+# Mutually Exclusive Probability
+event_A = [1, 2]
+event_B = [5, 6]
+total_outcomes = 6
+
+def probability(fav , total):
+    return len(fav) / total
+
+mutual_prob = probability(event_A, total_outcomes) + probability(event_B, total_outcomes)
+print("Mutually Exclusive Probability (A or B):", mutual_prob)
