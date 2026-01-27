0e4d221c73eda17849975ee89a7e03bdb8e8a84c
CodeQueen-AI <codeq209@gmail.com>
2026-01-27 15:19:26 +0500

    update

--- 1-Descriptive Statistics/Standard Deviation.py
+++ 1-Descriptive Statistics/Standard Deviation.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import math
 
 def standard_deviation(data):
@@ -12,13 +14,10 @@ marks = [80, 50, 45, 91]
 print('Standard Deviation of Marks is : ', standard_deviation(marks))
 
 # Using Numpy
-import numpy as np
-
 marks = [80, 50, 45, 91]
 print('Standard Deviation Numpy is :', np.std(marks))
 
 # Using Pandas
-import pandas as pd
 marks = [80, 50, 45, 91]
 series = pd.series(marks)
 print('Standard Deviation Pandas is' , series.std())
