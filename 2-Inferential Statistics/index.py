b9c2e53fd6c189c0c53c68969819e5d8896856f5
CodeQueen-AI <codeq209@gmail.com>
2026-01-27 18:23:14 +0500

    update

--- 2-Inferential Statistics/index.py
+++ 2-Inferential Statistics/index.py
@@ -40,18 +40,13 @@ def probability(fav , total):
 mutual_prob = probability(event_A, total_outcomes) + probability(event_B, total_outcomes)
 print("Mutually Exclusive Probability (A or B):", mutual_prob)
 
-# Simple Conditional Probability
-
-# Events Example
-event_A = [4, 5, 6]  # Event A
-event_B = [2, 4, 6]  # Event B
+# Conditional Probability
+event_A = [4, 5, 6] 
+event_B = [2, 4, 6] 
 total_outcomes = 6
 
-# Probability function
 def probability(fav, total):
     return len(fav) / total
 
-# Conditional Probability P(A|B)
 cond_prob = len(set(event_A) & set(event_B)) / len(event_B)
-
 print("Conditional Probability P(A|B):", cond_prob)
