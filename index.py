92c3354c7e1fef5b9a3e8b0861d98eb885d2fb6b
CodeQueen-AI <codeq209@gmail.com>
2026-01-28 18:46:42 +0500

    update

--- index.py
+++ index.py
@@ -1,3 +1,19 @@
 # Import
 import numpy as np
 from scipy.stats import norm, t
+
+# Find Critical Value
+def t_critical(df, alpha=0.5, tail='two'):
+    if tail == 'two':
+        alpha = alpha / 2
+        critical_value = t.ppf(1-alpha, df)
+        critical_value_2 = - critical_value
+        return (critical_value, critical_value_2)
+    elif tail == 'right':
+        critical_value = t.ppf(1-alpha, df)
+        return critical_value
+    elif tail == 'left':
+        critical_value = t.ppf(alpha, df)
+        return critical_value
+    else:
+        print("Tail must be 'two', 'right', or'left'")
